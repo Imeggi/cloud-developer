42a98bf09486fa73f61bdf59b7ff1f54221ee55a
Imeggi <carl.erik.andreason@gmail.com>
2020-03-21 23:09:00 +0100

    added environment variables

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "udagramandreas",
-    "password": "Havevi18109!!",
-    "database": "udagramandreas",
-    "host": "udagramandreas.cu5cfxlt3q7e.eu-west-1.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "eu-west-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-andreason-dev"
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": process.env.POSTGRESS_DIALECT,
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
@@ -17,6 +17,6 @@ export const config = {
     "dialect": "postgres"
   },
   "jwt": {
-    "jwt_secret": "helloWorld"
+    "jwt_secret": process.env.JWT_SECRET
   }
 }
