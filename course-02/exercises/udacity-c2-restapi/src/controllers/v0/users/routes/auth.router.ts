6b6ac015e04ab4c96802f842db37c3b57c61ed3f
Imeggi <carl.erik.andreason@gmail.com>
2020-03-21 23:27:01 +0100

    enabled authentication

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -5,46 +5,55 @@ import { User } from '../models/User';
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import { NextFunction } from 'connect';
+import { config } from '../../../../config/config'
 
 import * as EmailValidator from 'email-validator';
+
 
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(plainTextPassword, salt);
+
+
+    
     //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return;
+    return hash;
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return;
+    const passwordsEqual = await bcrypt.compare(plainTextPassword, hash);
+    return passwordsEqual;
 }
 
 function generateJWT(user: User): string {
     //@TODO Use jwt to create a new JWT Payload containing
-    return;
+
+    return jwt.sign(user.toJSON(), config.jwt.jwt_secret);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    
-     if (!req.headers || !req.headers.authorization){
+    if (!req.headers || !req.headers.authorization){
          return res.status(401).send({ message: 'No authorization headers.' });
      }
     
 
-     const token_bearer = req.headers.authorization.split(' ');
-     if(token_bearer.length != 2){
-         return res.status(401).send({ message: 'Malformed token.' });
-     }
+    const token_bearer = req.headers.authorization.split(' ');
+    if(token_bearer.length != 2){
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
     
-     const token = token_bearer[1];
+    const token = token_bearer[1];
 
-     return jwt.verify(token, "hello", (err, decoded) => {
-       if (err) {
-         return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-       }
+    return jwt.verify(token, config.jwt.jwt_secret, (err, decoded) => {
+      if (err) {
+       return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+      }
       return next();
-     });
+    });
 }
 
 router.get('/verification', 
@@ -123,7 +132,7 @@ router.post('/', async (req: Request, res: Response) => {
     // Generate JWT
     const jwt = generateJWT(savedUser);
 
-    res.status(201).send({token: jwt, user: savedUser.short()});
+    res.status(201).send({ auth: true, token: jwt, user: savedUser.short()});
 });
 
 router.get('/', async (req: Request, res: Response) => {
